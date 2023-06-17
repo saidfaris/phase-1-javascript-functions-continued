62ac77ece87521588ff52c607d4dc9bf0995d559
sidee <said.issack@student.moringaschool.com>
2023-06-17 23:16:38 +0300

    completed Assighnment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  console.log(`What do you want to do on Saturday? I suggest you ${activity}`);
+}
+
+function mondayWork(activity = 'go to the office') {
+  console.log(`What do you want to do on Monday? I suggest you ${activity}`);
+}
+
+function wrapAdjective(visualFlair = '*', adjective = 'special') {
+  return function(adjective) {
+    return `You are ${visualFlair}${adjective}`;
+  };
+}
